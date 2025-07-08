23c36b3df3a1d284833af8f0ae62388e0190aa20
Jhofran Briceño <jhofran.dev@outlook.com>
2025-07-08 11:39:32 -0500

    feat: add pretest script to clear database and enhance product validation tests

--- src/handlers/__test__/product.test.ts
+++ src/handlers/__test__/product.test.ts
@@ -2,6 +2,31 @@ import request from "supertest";
 import server from "../../server";
 
 describe("POSt /api/products", () => {
+  it("should display validation error", async () => {
+    const response = await request(server).post("/api/products").send({});
+
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty("errors");
+    expect(response.body.errors).toHaveLength(4);
+
+    expect(response.status).not.toBe(404);
+    expect(response.body.errors).not.toHaveLength(2);
+  });
+
+  it("should validate that the price is a number and greater than 0", async () => {
+    const response = await request(server).post("/api/products").send({
+      name: "Test Product",
+      price: "hola",
+    });
+
+    expect(response.status).toBe(400);
+    expect(response.body).toHaveProperty("errors");
+    expect(response.body.errors).toHaveLength(2);
+
+    expect(response.status).not.toBe(404);
+    expect(response.body.errors).not.toHaveLength(3);
+  });
+
   it("should create a new product", async () => {
     const response = await request(server).post("/api/products").send({
       name: "Test Product",
@@ -14,5 +39,23 @@ describe("POSt /api/products", () => {
     expect(response.status).not.toBe(400);
     expect(response.status).not.toBe(404);
     expect(response.body).not.toHaveProperty("error");
+  });
+});
+
+describe("GET /api/products", () => {
+  it("should check if api/products url exists", async () => {
+    const response = await request(server).get("/api/products");
+
+    expect(response.status).not.toBe(404);
+  });
+
+  it("Get a JSON response with products", async () => {
+    const response = await request(server).get("/api/products");
+
+    expect(response.status).toBe(200);
+    expect(response.headers["content-type"]).toMatch(/json/);
+    expect(response.body).toHaveProperty("data");
+    expect(response.body.data).toHaveLength(1);
+    expect(response.body).not.toHaveProperty("errors");
   });
 });
